3336428953f16dffdeed747166fae9d4a5c2e18e
cristinagiacobbe <crigiak@gmail.com>
2024-01-22 23:11:56 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "Questo è il risultato della faticosa impresa di stampare in pagina un messaggio usando Vuejs",
+      image: "./emoticon.jpg"
+    }
+
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <img src="./emoticon.jpg" alt="immagine non disponibile">
+</template>
+
+<style></style>
